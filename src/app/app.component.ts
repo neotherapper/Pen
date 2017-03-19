dbcc31a7af991f4cb588f61519a78ae869edc01a
GEORGE PILITSOLOU <george.pilitsoglou@gmail.com>
2017-03-19 20:44:18 +0000

    Added firebase authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
+import {AngularFire} from 'angularfire2';
+import User = firebase.User;
+const firebase = require('firebase');
+const firebaseui = require('firebaseui');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  private firebaseUiInstance;
   title = 'app works!';
+
+
+  constructor(private aF: AngularFire) {
+    // Initialize the FirebaseUI Widget using Firebase.
+    this.firebaseUiInstance = new firebaseui.auth.AuthUI(firebase.auth());
+
+  }
+
+  ngAfterViewInit(): void {
+
+    // Only fire this if the user isn't authenticated
+    this.aF.auth.subscribe(value => {
+      if (!value) {
+        this.firebaseUIPopup();
+      }
+    });
+
+    firebase.auth().onAuthStateChanged(this.firebaseAuthChangeListener, (error) => console.log(error));
+
+  }
+
+  firebaseUIPopup() {
+    const uiConfig = {
+      callbacks: {
+        signInSuccess: (currentUser, credential, rederictUrl) => {
+          return false;
+        }
+      },
+      signInFlow: 'popup',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '<your-tos-url>'
+    };
+
+    // The start method will wait until the DOM is loaded.
+    this.firebaseUiInstance.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  firebaseAuthChangeListener(user: User) {
+    if (user) {
+      user.getToken().then(function (accessToken) {
+        document.getElementById('account-details').textContent = JSON.stringify({
+          displayName: user.displayName,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          photoURL: user.photoURL,
+          uid: user.uid,
+          accessToken: accessToken,
+          providerData: user.providerData
+        }, null, '  ');
+      });
+    } else {
+      document.getElementById('account-details').textContent = 'null - User not signed in';
+    }
+  }
+
+  logout() {
+    this.aF.auth.logout().then(() => console.log(this.aF.auth.subscribe(v => console.log(v))));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 // 3rd party
+import { AngularFireModule } from 'angularfire2';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
 // App components
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAB21wYyQshHxkgyWdB6_oNUW-Ubggony4',
+  authDomain: 'pens-ed338.firebaseapp.com',
+  databaseURL: 'https://pens-ed338.firebaseio.com',
+  storageBucket: 'pens-ed338.appspot.com',
+  messagingSenderId: '725515076346'
+};
 
 @NgModule({
   declarations: [
@@ -16,6 +26,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule,
     MaterialModule
